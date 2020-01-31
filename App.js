35fa0b960bb7a3593e378c77651ac06889a43de8
alex-png <alexrod102797@gmail.com>
2020-01-31 00:02:06 -0500

    touching

--- App.js
+++ App.js
@@ -1,29 +1,67 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+export default class Touchables extends Component {
+  _onPressButton() {
+    alert('You tapped the button!')
+  }
+
+  _onLongPressButton() {
+    alert('You long-pressed the button!')
+  }
+
+
   render() {
     return (
-      
-      <View style={{
-        flex: 1,
-        flexWrap: 'wrap'
-      }}>
-        <View style={{width: 50, height: 100, backgroundColor: 'powderblue'}} />
-        <View style={{height: 100, width: 50, backgroundColor: 'skyblue'}} />
-        <View style={{height: 100, width: 50, backgroundColor: 'steelblue'}} />
-        <View style={{width: 50, height: 100, backgroundColor: 'powderblue'}} />
-        <View style={{height: 100, width: 50, backgroundColor: 'skyblue'}} />
-        <View style={{height: 100, width: 50, backgroundColor: 'steelblue'}} />
-        <View style={{width: 50, height: 100, backgroundColor: 'powderblue'}} />
-        <View style={{height: 100, width: 50, backgroundColor: 'skyblue'}} />
-        <View style={{height: 100, width: 50, backgroundColor: 'steelblue'}} />
-        <View style={{width: 50, height: 100, backgroundColor: 'powderblue'}} />
-        <View style={{height: 100, width: 50, backgroundColor: 'skyblue'}} />
-        <View style={{height: 100, width: 50, backgroundColor: 'steelblue'}} />
-
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableHighlight</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableOpacity onPress={this._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableOpacity</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableNativeFeedback
+            onPress={this._onPressButton}
+            background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableNativeFeedback {Platform.OS !== 'android' ? '(Android only)' : ''}</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <TouchableWithoutFeedback
+            onPress={this._onPressButton}
+            >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Touchable with Long Press</Text>
+          </View>
+        </TouchableHighlight>
       </View>
- 
     );
   }
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    alignItems: 'center'
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'white'
+  }
+});
